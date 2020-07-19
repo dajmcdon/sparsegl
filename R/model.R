7fd364ed0e6c697e82fec3f7262b8fc4021ced2e
Daniel McDonald <dajmcdon@gmail.com>
2020-07-19 16:51:09 -0700

    add back utilities

--- R/model.R
+++ R/model.R
@@ -54,11 +54,22 @@ sparsegl <- function(
 
 
 gglasso <- function(bn, bs, ix, iy, nobs, nvars, x, y, pf, dfmax,
-    pmax, nlam, flmin, ulam, eps, maxit, vnames, group, intr) {
+    pmax, nlam, flmin, ulam, eps, maxit, vnames, group, intr, standardize) {
     #################################################################################
     # call Fortran core
+    intercept = 0L
+    if(intr){
+        ym = mean(y)
+        xm = colMeans(x)
+        x = sweep(x,2,xm)
+        y = y-ym
+    }
+    if(standardize){
+        xs = sqrt(colSums(x^2))
+        x = sweep(x,2,xs,"/")
+    }
     gamma <- rep(NA, bn)
-    for (g in 1:bn) gamma[g] <- max(eigen(crossprod(x[, ix[g]:iy[g]]))$values)
+    for (g in 1:bn) gamma[g] <- RSpectra::svds(x[,ix[g]:iy[g]],1,0,0)$d^2
     gamma <- gamma/nobs
     gamma <- as.double(gamma)
     fit <- .Fortran("gglasso", bn, bs, ix, iy, gamma, nobs, nvars, as.double(x),
@@ -68,6 +79,12 @@ gglasso <- function(bn, bs, ix, iy, nobs, nvars, x, y, pf, dfmax,
     #################################################################################
     # output
     outlist <- getoutput(fit, maxit, pmax, nvars, vnames)
+    if(standardize){
+        outlist$beta = outlist$beta/xs
+    }
+    if(intr){
+        outlist$b0 = ym - xm %*% outlist$beta
+    }
     outlist <- c(outlist, list(npasses = fit$npass, jerr = fit$jerr, group = group))
     class(outlist) <- c("ls")
     outlist
