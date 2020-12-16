c32803dc37e872f0ef4fd31972197656394b2421
Daniel McDonald <dajmcdon@gmail.com>
2020-12-15 16:27:21 -0800

    updates for andy

--- R/model.R
+++ R/model.R
@@ -2,30 +2,34 @@ sgl <- function(
     bn, bs, ix, iy, nobs, nvars, x, y, pf, dfmax, pmax, nlam, flmin, ulam, eps,
     maxit, vnames, group, intr, asparse, standardize, algorithm) {
     # call Fortran core
-    intercept = 0L
-    is.sparse=FALSE
-    if(inherits(x,"sparseMatrix")){##Sparse case
-        is.sparse=TRUE
-        x=as(x,"CsparseMatrix")
-        x=as(x,"dgCMatrix")
-        xidx=as.integer(x@i+1)
-        xcptr=as.integer(x@p+1)
-        xval=as.double(x@x)
-        nnz=as.integer(tail(x@p,1))
+    intercept <- 0L
+    is.sparse <- FALSE
+    if (inherits(x,"sparseMatrix")) {
+        is.sparse <- TRUE
+        x <- as(x,"CsparseMatrix")
+        x <- as(x,"dgCMatrix")
+        xidx <- as.integer(x@i + 1)
+        xcptr <- as.integer(x@p + 1)
+        xval <- as.double(x@x)
+        nnz <- as.integer(tail(x@p, 1))
         algorithm="fourstepspx"
+        if (intr) {
+            ym <- mean(y)
+            y <- y - ym
+        }
     }
-    if(intr && !is.sparse){
-        ym = mean(y)
-        xm = colMeans(x)
-        x = sweep(x,2,xm)
-        y = y-ym
+    if (intr && !is.sparse) {
+        ym <- mean(y)
+        xm <- colMeans(x)
+        x <- sweep(x,2,xm)
+        y <- y - ym
     }
-    if(standardize){
-        xs = 1/sqrt(Matrix::colSums(x^2))
-        x = x %*% Matrix::Diagonal(x=xs)
+    if (standardize) {
+        xs <- 1 / sqrt(Matrix::colSums(x^2))
+        x <- x %*% Matrix::Diagonal(x = xs)
     }
     gamma <- rep(NA, bn)
-    for (g in 1:bn) gamma[g] <- RSpectra::svds(x[,ix[g]:iy[g]],1,0,0)$d^2
+    for (g in 1:bn) gamma[g] <- RSpectra::svds(x[,ix[g]:iy[g]], 1, 0, 0)$d^2
     gamma <- gamma/nobs
     gamma <- as.double(gamma)
     fit <- switch(algorithm,
@@ -45,18 +49,22 @@ sgl <- function(
         fourstepspx = .Fortran(
             "spmat_four", bn,bs,ix,iy,gamma,nobs,nvars,xval,xidx,xcptr,nnz,
             as.double(y), pf, dfmax, pmax, nlam, flmin, ulam, eps, maxit,
-            as.integer(intr), nalam=0L, b0=double(nlam), beta=double(nvars*nlam),
+            as.integer(intr), nalam=0L, b0=double(nlam),beta=double(nvars*nlam),
             activeGroup=integer(pmax),nbeta=integer(nlam),alam=double(nlam),
             npass=0L,jerr=0L,alsparse=as.double(asparse)),
         stop("Requested algorithm is not implemented.")
         )
     # output
     outlist <- getoutput(fit, maxit, pmax, nvars, vnames)
-    if(standardize){
+    if (standardize) {
         outlist$beta = outlist$beta/xs
     }
-    if(intr && !is.sparse){
-        outlist$b0 = ym - xm %*% outlist$beta
+    if (intr) {
+        if (is.sparse) {
+            outlist$b0 <- outlist$b0 + ym
+        } else {
+            outlist$b0 <- ym - xm %*% outlist$beta
+        }
     }
     outlist <- c(outlist, list(npasses = fit$npass, jerr = fit$jerr, group = group))
     class(outlist) <- c("ls")
