f87005781f920b5c7590b3560fd068948faee163
Daniel J. McDonald <dajmcdon@gmail.com>
2021-03-03 12:22:48 -0800

    ditch the data

---
+++ tests/testthat.R
@@ -0,0 +1,4 @@
+library(testthat)
+library(sparsegl)
+
+test_check("sparsegl")
---
+++ tests/testthat/test-sparsegl.R
@@ -0,0 +1,11 @@
+test_that("multiplication works", {
+
+
+
+  expect_equal(2 * 2, 4)
+})
+
+
+test_that("subtraction works", {
+  expect_equal(2-2, 1)
+})
