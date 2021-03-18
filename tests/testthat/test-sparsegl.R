0448c6c0dc791b5ce680195ffb2282a2e1dc17fa
Xiaoxuan Liang <xiaoxuanliang@Xiaoxuans-iMac.local>
2021-03-17 23:03:10 -0700

    tests

--- tests/testthat/test-sparsegl.R
+++ tests/testthat/test-sparsegl.R
@@ -1,4 +1,6 @@
 library(Matrix)
+library(glmnet)
+library(gglasso)
 
 test_that("multiplication works", {
   expect_equal(2 * 2, 4)
@@ -10,7 +12,7 @@ test_that("x is not a matrix", {
   y <- c(1,2)
   expect_false(is.matrix(x))
   expect_false(inherits(x, "sparseMatrix"))
-  expect_error(sparsegl(x, y),"x has to be a matrix" )
+  expect_error(sparsegl(x, y) )
 })
 
 test_that("x is not a sparse matrix",{
@@ -29,7 +31,7 @@ test_that("some entries in x is missing", {
   x[1,1] <- NA
   y <- c(1,2)
   expect_true(any(is.na(x)))
-  expect_error(sparsegl(x, y), "Missing values in x not allowed!")
+  expect_error(sparsegl(x, y))
 })
 
 
@@ -38,7 +40,7 @@ test_that("x and y have different dimensions", {
   i <- c(1,3:8); j <- c(2,9,6:10); x <- 7 * (1:7)
   A <- sparseMatrix(i, j, x = x)
   y = c(1,2)
-  expect_error(sparsegl(A,y), "x and y have different number of rows")
+  expect_error(sparsegl(A,y))
 })
 
 test_that("y is not numeric", {
@@ -47,7 +49,7 @@ test_that("y is not numeric", {
   y <- c("1", "2")
   expect_type(y, "character")
   expect_false(is.numeric(y))
-  expect_error(sparsegl(x, y), "The response y must be numeric.")
+  expect_error(sparsegl(x, y))
 })
 
 
@@ -65,24 +67,88 @@ test_that("parameter issues", {
   
   x <- matrix(c(1,2,3,4,5,6), nrow = 2)
   y <- c(1,2)
-  expect_error(sparsegl(x, y, group = c(1,2)), "group length does not match the number of predictors in x")
-  expect_error(sparsegl(x, y, group = c(1,2,4)), "Groups must be consecutively numbered 1,2,3,...")
-  expect_error(sparsegl(x, y, group = c(2,3,4)), "Groups must be consecutively numbered 1,2,3,...")
+  expect_error(sparsegl(x, y, group = c(1,2)))
+  expect_error(sparsegl(x, y, group = c(1,2,4)))
+  expect_error(sparsegl(x, y, group = c(2,3,4)))
   expect_warning(sparsegl(x, y, group = c(1,2,3), asparse = 1.1))
   expect_warning(sparsegl(x, y, group = c(1,2,3), asparse = -1))
-  expect_error(sparsegl(x, y, pf = c(1,2)), "The length of group-lasso penalty factor must be same as the number of groups")
-  expect_error(sparsegl(x, y, lambda.factor = 2), "lambda.factor should be less than 1")
-  expect_error(sparsegl(x, y, lambda = c(-1, 1)),"lambdas must be non-negative" )
-  expect_error(sparsegl(x, y, lower_bnd = c(-1, -5, 2)), "Lower bounds should be non-positive")
-  expect_error(sparsegl(x, y, upper_bnd = c(-1, -5, 2)), "Upper bounds should be non-negative")
-  expect_error(sparsegl(x, y, lower_bnd = c(-1, -5)), "Lower bounds must be length 1 or length the number of groups")
-  expect_error(sparsegl(x, y, upper_bnd = c(1,5)), "Upper bounds must be length 1 or length the number of groups")
+  expect_error(sparsegl(x, y, pf = c(1,2)))
+  expect_error(sparsegl(x, y, lambda.factor = 2))
+  expect_error(sparsegl(x, y, lambda = c(-1, 1)))
+  expect_error(sparsegl(x, y, lower_bnd = c(-1, -5, 2)))
+  expect_error(sparsegl(x, y, upper_bnd = c(-1, -5, 2)))
+  expect_error(sparsegl(x, y, lower_bnd = c(-1, -5)))
+  expect_error(sparsegl(x, y, upper_bnd = c(1,5)))
 })
 
+test_that("no penalty", {
+  
+  X <- matrix(c(1,2,3,4,5,6), nrow = 3)
+  y <- c(1.5, 3.5, 5.5)
+  res1 <- sparsegl(X, y, lambda = 0)
+  res2 <- summary(lm(y~X))
+  
+  expect_equal(res1$b0[1], res2$coefficients[1,1])
+  expect_equal(res1$beta[1], res2$coefficients[2,1])
+  expect_equal(res1$beta[2], 0)
+  
+  # print(res1$b0)
+  # print(res1$beta)
+  # print(res2$coefficients)
+  
+})
+
+test_that("lasso penalty", {
+  
+  X <- matrix(c(1,2,3,4,5,6), nrow = 3)
+  y <- c(2,4,6)
+  
+  # try large lambda
+  expect_equal(sparsegl(X, y, lambda = 2, asparse = 1)$beta[1:2],c(0,0))
+  
+  fit <- glmnet(X, y, alpha = 1)
+  lambda <- fit$lambda[c(1:2, (length(fit$lambda) - 1) :length(fit$lambda))]
+  # check the first two lambdas and last two lambdas extracted from glmnet
+  
+  for (i in 1 : length(lambda)){
+    fit1 <- sparsegl(X, y, asparse = 1, lambda = lambda[i])
+    fit2 <- glmnet(X, y, alpha = 1)
+    print(paste("lambda = ", lambda[i]))
+    print(fit1$b0)
+    print(fit1$beta)
+    print(coef(fit2, s = lambda[i]))
+  }
+})
+
+test_that("group lasso penalty", {
+  
+  set.seed(20200110)
+  n <- 100
+  p <- 48
+  g <- 6
+  X <- matrix(rnorm(n*p), nrow = n)
+  for (i in seq(1, p, by = 6))
+    X[, i:(i + g - 1)] <- svd(X[, i:(i + g - 1)])$u
+  beta <- c(rep(5, 6), rep(-5, 6), rep(0, p - 12))
+  y <- X %*% beta + rnorm(n, sd = .1)
+  
+  group <-  rep(1:16,each=3)
+  fit <- gglasso(x = X, y = y, group = group)
+  lambda = fit$lambda[1:5]
+  
+  # try large lambda
+  expect_equal(sparsegl(X, y, lambda = 2, asparse = 0, group = group)$beta[1:48], c(rep(0,48)))
 
 
-
-
+  for (i in 1:length(lambda)){
+    fit1 <- sparsegl(X, y, asparse = 0, lambda = lambda[i], group = group)
+    fit2 <- gglasso(X, y, group = group)
+    print(paste("lambda = ", lambda[i]))
+    print(fit1$b0)
+    print(fit1$beta)
+    print(coef(fit2, s = lambda[i]))
+  }
+})
 
 
 
