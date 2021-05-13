9f6ae9470b4fab5bd3bcb3f71a17fb0efc7b6692
Xiaoxuan Liang <doluxic@gmail.com>
2021-05-13 15:06:48 -0700

    modify base plot to ggplot

--- R/plot.cv.sparsegl.R
+++ R/plot.cv.sparsegl.R
@@ -38,20 +38,30 @@
 #' @export
 plot.cv.sparsegl <- function(x, sign.lambda = 1, ...) {
     cvobj <- x
-    xlab <- "log(Lambda)"
-    if (sign.lambda < 0) 
-        xlab <- paste("-", xlab, sep = "")
-    plot.args <- list(x = sign.lambda * log(cvobj$lambda), y = cvobj$cvm, ylim = range(cvobj$cvupper, 
-        cvobj$cvlo), xlab = xlab, ylab = cvobj$name, type = "n")
-    new.args <- list(...)
-    if (length(new.args)) 
-        plot.args[names(new.args)] <- new.args
-    do.call("plot", plot.args)
-    error.bars(sign.lambda * log(cvobj$lambda), cvobj$cvupper, cvobj$cvlo, width = 0.01, 
-        col = "darkgrey")
-    points(sign.lambda * log(cvobj$lambda), cvobj$cvm, pch = 20, col = "red")
-    axis(side = 3, at = sign.lambda * log(cvobj$lambda), tick = FALSE, line = 0)
-    abline(v = sign.lambda * log(cvobj$lambda.min), lty = 3)
-    abline(v = sign.lambda * log(cvobj$lambda.1se), lty = 3)
-    invisible()
+    # xlab <- "log(Lambda)"
+    # if (sign.lambda < 0)
+    #     xlab <- paste("-", xlab, sep = "")
+    # plot.args <- list(x = sign.lambda * log(cvobj$lambda), y = cvobj$cvm, ylim = range(cvobj$cvupper,
+    #     cvobj$cvlo), xlab = xlab, ylab = cvobj$name, type = "n")
+    # new.args <- list(...)
+    # if (length(new.args))
+    #     plot.args[names(new.args)] <- new.args
+    # do.call("plot", plot.args)
+    # error.bars(sign.lambda * log(cvobj$lambda), cvobj$cvupper, cvobj$cvlo, width = 0.01,
+    #     col = "darkgrey")
+    # points(sign.lambda * log(cvobj$lambda), cvobj$cvm, pch = 20, col = "red")
+    # axis(side = 3, at = sign.lambda * log(cvobj$lambda), tick = FALSE, line = 0)
+    # abline(v = sign.lambda * log(cvobj$lambda.min), lty = 3)
+    # abline(v = sign.lambda * log(cvobj$lambda.1se), lty = 3)
+    # invisible()
+    
+    dat <- data.frame("X" = sign.lambda * log(cvobj$lambda), 
+                      "y" = cvobj$cvm, 
+                      "upper" = cvobj$cvupper,
+                      "lower" = cvobj$cvlo)
+    dat %>% ggplot2::ggplot(aes(x = X, y = y)) +
+        geom_point(color = 'red') + 
+        geom_errorbar(aes(ymin = lower, ymax = upper), width = 0.1, color = 'darkgrey') + 
+        xlab("Log Lambda") + 
+        ylab(cvobj$name)
 } 
