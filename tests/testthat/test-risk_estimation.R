ec5514461ba69ed51a25eb4e5d5f6b9a4766be40
Xiaoxuan Liang <doluxic@gmail.com>
2021-05-14 16:52:30 -0700

    add test for risk_estimation

--- tests/testthat/test-risk_estimation.R
+++ tests/testthat/test-risk_estimation.R
@@ -1,3 +1,11 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+
+test_that("test for exact_df function", {
+  n <- 100
+  beta <- c(5,5,5,-5,-5,-5,1,0,1,0,0,0,0,2,0)
+  gr <- rep(1:5, each = 3)
+  X <- matrix(rnorm(n * length(beta)), n)
+  y <- X %*% beta + rnorm(n)
+  out <- sparsegl(X, y, gr)
+  
+  expect_equal(exact_df(out, X), as.vector(out$df), tolerance = 0.01)
 })
