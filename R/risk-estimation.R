63159c46b109a06b639d12af2e0b80f2a8b0df80
Xiaoxuan Liang <doluxic@gmail.com>
2021-06-04 13:52:28 -0700

    only change delP function

--- R/risk-estimation.R
+++ R/risk-estimation.R
@@ -70,6 +70,7 @@ delP <- function(beta, group) {
     p <- length(x)
     bn <- two_norm(x)
     Matrix::diag(1/bn, nrow = p, ncol = p) - outer(x / bn, x)
+    #(Matrix::diag(x / bn, nrow = p, ncol = p) - outer(x, x)) / bn
   })
   return(Matrix::bdiag(mats))
 }
@@ -81,38 +82,3 @@ sp_group_norm <- function(x, gr, asparse = 0.05) {
   asparse * sum(abs(x)) + (1 - asparse) * gr_norm(x, gr)
 }
 
-
-exact_df <- function(object, x) {
-  # See the correct formula in https://arxiv.org/pdf/1212.6478.pdf
-  # Theorem 2
-  Iset <- abs(object$beta) > 0
-  Imax <- which(apply(Iset, 1, any))
-  Iset <- Iset[Imax,]
-  group <- object$group[Imax]
-  beta <- object$beta[Imax,]
-  nlambda <- length(object$lambda)
-  xx <- x[,Imax]
-  df <- double(nlambda)
-  for (i in seq(nlambda)) {
-    Idx <- Iset[, i]
-    if (any(Idx)) {
-      X_I <- xx[, Idx]
-      del <- delP(beta[Idx, i], group[Idx])
-      df[i] <- sum(diag(X_I %*% solve(t(X_I) %*% X_I + object$lambda[i] * del) %*% t(X_I)))
-    } else {
-      df[i] <- 0
-    }
-  }
-  return(df)
-}
-
-
-delP <- function(beta, group) {
-  betas <- split(beta, group)
-  mats <- lapply(betas, function(x) {
-    p <- length(x)
-    bn <- two_norm(x)
-    (Matrix::diag(x / bn, nrow = p, ncol = p) - outer(x, x)) / bn
-  })
-  return(Matrix::bdiag(mats))
-} 
