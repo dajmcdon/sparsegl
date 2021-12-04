0516aaf48e466e47a58a8978d275d321f437784f
Daniel J. McDonald <dajmcdon@gmail.com>
2021-12-04 13:53:35 -0800

    pass checks, remove spurious imports

--- R/imports.R
+++ R/imports.R
@@ -1,7 +1,5 @@
 #' @useDynLib sparsegl, .registration = TRUE
 #' @importFrom utils head tail packageDescription
-#' @importFrom grDevices rainbow
-#' @importFrom graphics abline axis par plot.default points segments
 #' @importFrom methods cbind2 rbind2
 #' @importFrom stats approx coef predict
 #' @importFrom rlang .data
