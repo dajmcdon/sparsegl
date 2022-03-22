dfebe3ae5b0b35b524d457aac254e71ac716393a
Xiaoxuan1121 <doluxic@gmail.com>
2022-03-22 12:29:01 -0700

    add more tests

--- tests/testthat/test-l1penalty.R
+++ tests/testthat/test-l1penalty.R
@@ -35,3 +35,32 @@ test_that("function will rescale each entry such that the sum will be constant",
   expect_equal(out1$lambda, out2$lambda)
   
 })
+
+test_that("function behaviors changed by pfl1", {
+  n <- 100
+  beta <- c(5, 5 , 5, -5, -5, -5)
+  gr <- rep(1:2, each = 3)
+  X <- matrix(rnorm(n * length(beta)), n)
+  y <- X %*% beta + rnorm(n)
+  pfl1_1 <- c(rep(0, 3), rep(10, 3))
+  pfl1_2 <- c(rep(0, 3), rep(20, 3))
+  out1 <- sparsegl(X, y, group = gr, pfl1 = pfl1_1)
+  out2 <- sparsegl(X, y, group = gr, pfl1 = pfl1_2)
+  
+  expect_equal(out1$b0, out2$b0)
+  expect_equal(out1$beta, out2$beta)
+  expect_equal(out1$lambda, out2$lambda)
+  
+  plf1_2 <- c(rep(1, 14), 10)
+  out1 <- sparsegl(X, y, group = gr)
+  out2 <- sparsegl(X, y, group = gr, pfl1 = pfl1_2)
+  
+  # predictor 15 has larger coefficient in model out1 than in out2
+  expect_equal(as.numeric(abs(out1$beta[6, ]) >= abs(out2$beta[6, ])), 
+               rep(1, 100))
+  
+  # lambda is larger when the coefficient of predictor15 turns nonzero in out1
+  expect_true(out1$lambda[which(out1$beta[4, ] != 0)[1]] >= 
+                out2$lambda[which(out2$beta[4, ] != 0)[1]])
+})
+
