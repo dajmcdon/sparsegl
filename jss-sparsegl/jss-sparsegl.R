4d858c5a4fcb07df1eca3b5513d451d1fb56e041
Daniel J. McDonald <dajmcdon@gmail.com>
2022-08-04 14:04:54 -0700

    all reproduction code in 1 script

--- jss-sparsegl/jss-sparsegl.R
+++ jss-sparsegl/jss-sparsegl.R
@@ -1,22 +1,70 @@
-## ---- setup, include=FALSE---------------------------------------------------------
-knitr::opts_chunk$set(echo = TRUE, cache = TRUE, fig.path = "fig/")
-options(prompt = 'R> ', continue = '+ ')
-ggplot2::theme_set(ggplot2::theme_bw(base_family = "Palatino"))
-# Add any packages we want to cite to the list below. Don't edit the bib
-# manually, or changes will be overwritten. Add other references to
-# `sparsegl.bib`
+# _Note on replication_
 #
-# Note: tags are R-<pkg>
-knitr::write_bib(c("devtools", "knitr", "testthat", "usethis",
-                   "rticles", "sparsegl", "glmnet",
-                   "SGL","gglasso", "msgl", "biglasso",
-                   "RSpectra",
-                   "ggplot2", "dotCall64"),
-                 file = "pkgs.bib")
+# This file will reproduce all results, but downloading the large brain data
+# and running the timing simulation at the beginning takes some time/memory.
+# For this reason, we have provided the results of these experiments in
+# `large-data.zip`. The reproduction code is commented out, while the
+# `large-data.zip` file is expected to be extracted in the same directory as
+# this script.
+
+
+# Necessary packages
+# install.packages(c("knitr", "ggplot2", "tibble", "SGL", "dplyr", "covidcast"))
+# install.packages("scales")
+#
+
+## ---- setup, include=FALSE----------------------------------------------------
+## Used in the Rmd for the pdf submission
+# knitr::opts_chunk$set(echo = TRUE, cache = TRUE, fig.path = "fig/")
+# options(prompt = 'R> ', continue = '+ ')
+ggplot2::theme_set(ggplot2::theme_bw(base_family = "Palatino"))
+
 
 
 ## ----timing-comparison, echo=FALSE, fig.width=6, fig.height=3, fig.cap="This figure shows the time required to compute sparse group lasso solutions across a number of different problem sizes. In all cases, we use $n=500$ observations and 100 values of the tuning parameter $\\lambda$. The median is taken across 5 replications for each method and problem size. Note that both axes are on the log scale."----
-# source("code/timing.R)
+# library(Matrix)
+# library(tibble)
+# library(SGL)
+# library(sparsegl)
+#
+# nrepls <- 5
+# n <- 500
+# p <- c(100, 250, 500, 1000, 2500)
+# b <- c(rep(1, 5), rep(-1, 5), c(-1,-1,0,0,0), c(1,1,0,0,0), rep(1, 5),
+#        rep(0, 75)) * 10
+# s <- .1
+# gr <- rep(1:(max(p) / 5), each = 5)
+# x <- matrix(rnorm(n*max(p)), nrow = n)
+# x[as.logical(rbinom(n*max(p), 1, 1 - s))] <- 0
+# xsp <- Matrix(x)
+# mu <- sapply(p, function(z) x[ ,1:z] %*% rep(b, length.out = z) / sqrt(z))
+# # prob <- 1 / (1 + exp(-mu))
+#
+# signal <- sqrt(colSums(mu^2))
+# noise_sd <- sqrt(signal)
+#
+# y <- mu + rnorm(n*length(p), sd = rep(noise_sd, each = n))
+# res <- tibble(method = "a", time = proc.time()["elapsed"], .rows = 0)
+# for (i in seq_along(p)) {
+#   pp <- seq(p[i])
+#   dat <- list(y = y[ ,i], x = x[ ,pp])
+#   xxsp <- xsp[ ,pp]
+#   g <- gr[pp]
+#   for (j in seq(nrepls)) {
+#     s1 <- system.time(
+#       SGL(dat, nlam = 100, alpha = 0.05, index = g, standardize = FALSE))
+#     res <- add_row(res, method = "SGL", time = s1["elapsed"])
+#     s2 <- system.time(sparsegl(dat$x, dat$y, g, standardize = FALSE))
+#     res <- add_row(res, method = "sparsegl", time = s2["elapsed"])
+#     s3 <- system.time(sparsegl(xxsp, dat$y, g, standardize = FALSE))
+#     res <- add_row(res, method = "sparsegl_sp", time = s3["elapsed"])
+#     print(paste("Done with p = ", p[i], "repl = ", j))
+#   }
+# }
+#
+# res$p <- rep(p, each = nrepls * 3)
+# saveRDS(res, "large-data/sparsegl-timing.rds")
+library(dplyr)
 res <- readRDS("large-data/sparsegl-timing.rds")
 res <- res %>%
   group_by(method, p) %>%
@@ -40,7 +88,6 @@ ggplot(res, aes(p, time, colour = method)) +
 
 ## ----install, eval=FALSE, echo=TRUE------------------------------------------------
 ## install.packages("sparsegl")
-## library(sparsegl)
 
 
 ## ---- data-simulation--------------------------------------------------------------
@@ -101,7 +148,6 @@ er <- estimate_risk(fit, X)
 
 
 ## ----re-plot, echo=FALSE, message=FALSE, warning=FALSE, fig.width = 4, fig.height = 2, fig.cap="AIC, BIC, and GCV (solid lines) along with their minima (vertical dashed lines)."----
-library(tidyverse)
 er <- er %>% dplyr::select(-df) %>% pivot_longer(-lambda, values_to = "risk")
 err <- er %>% group_by(name) %>% summarise(lambda = lambda[which.min(risk)])
 ggplot(er, aes(lambda, risk, color = name)) +
@@ -117,16 +163,62 @@ ggplot(er, aes(lambda, risk, color = name)) +
 
 
 ## ----trust, echo=FALSE, fig.cap="State-level estimates for the amount of trust in experts about Covid-19. The value displayed represents the change relative the US-wide average.", fig.width=8, fig.height=4, message=FALSE, warning=FALSE, out.width="5in"----
-source("code/covid.R")
+data("trust_experts")
+
+y <- trust_experts[,"y"]
+x <- trust_experts[,-1]
+gr <- c(rep(1:7, times = c(8, 51, 5, 4, 8, 10, 10)))
+fit <- sparsegl(x, y, gr)
+er <- estimate_risk(fit, x, approx_df = FALSE)
+cc <- coef(fit, s = er$lambda[which.min(er$BIC)])
+states <- tibble(state = rownames(cc)[10:60], coef = cc[10:60]) %>%
+  mutate(state_name = tolower(covidcast::abbr_to_name(state, TRUE)))
+states_map <- map_data("state")
+g <- ggplot(states, aes(map_id = state_name)) +
+  geom_map(aes(fill = coef), map = states_map) +
+  expand_limits(x = states_map$long, y = states_map$lat) +
+  scale_fill_gradient2(
+    low = "darkorange", high = "darkblue",
+    trans = scales::pseudo_log_trans(),
+    breaks = c(-10, -5, -2, 0, 2, 5, 10, 20),
+  ) +
+  coord_map(projection = "albers", parameters = c(30,40)) +
+  theme_void() +
+  theme(legend.position = "bottom", legend.key.width = unit(2, "cm"),
+        legend.title = element_blank())
 g
 
 
 ## ----dwi-model-description, out.width="85%", fig.cap="Pictorial representation illustrating how the streamlines and voxels are converted from a diffusion-weighted image to a linear model. Each voxel is measured on 90 angles, so it occupies 90 rows in the data. When a streamline (column of $\\mathbf{X}$) passes through a voxel, the values within that voxel are given by a physical model based on the direction of passage. Otherwise, if the streamline does not cross the voxel, the respective rows are zero.", echo=FALSE----
-knitr::include_graphics("fig/model-description.pdf")
+# knitr::include_graphics("fig/model-description.pdf")
 
 
 ## ----dwi-fit, echo=FALSE, warning=FALSE, eval=TRUE, fig.cap="The group norm of the 12 groups based on neuroanatomical structure against the magnitude of the penalty. The fit was produced using \\texttt{sparsegl()} to estimate the group lasso ($\\alpha=0$).", fig.height=3----
-# source("code/brains.R") takes 6 GB memory and about 1 minute, once the data is downloaded
+# Download and load brain data ---------------------------------------------------------
+# download <- function(url, path) {
+#   file.exists(path) || download.file(url, path, mode = 'wb')
+# }
+#
+# options(timeout = max(15 * 60)) # 15 minutes, assuming 1MB/s download speed
+# # https://doi.org/10.6084/m9.figshare.20314917
+# download("https://figshare.com/ndownloader/files/36288819", "large-data/A.mtx.gz")
+# download("https://figshare.com/ndownloader/files/36288825", "large-data/Y.rds")
+# download("https://figshare.com/ndownloader/files/36288822", "large-data/G.rds")
+# download("https://figshare.com/ndownloader/files/36294165", "large-data/Gpf.rds")
+# download("https://figshare.com/ndownloader/files/36288828", "large-data/brain-fit.rds")
+#
+# # The below files exceed the limits for JSS submission. We save only the result.
+# A <- Matrix::readMM("large-data/A.mtx.gz")
+# Y <- readRDS("large-data/Y.rds")
+# G <- readRDS("large-data/G.rds")
+# gpf <- readRDS("large-data/Gpf.rds")
+#
+# # Fit and estimate risk ---------------------------------------------------
+# system.time(fit <- sparsegl(A, Y, group=G, pf_group=gpf, asparse=0.0)) # ~ 1.5 minutes
+#
+# df <- estimate_risk(fit, A, approx_df=TRUE)
+# write.csv(df, "large-data/fit.csv", row.names=FALSE, quote=FALSE) # unused
+# saveRDS(fit, file = "large-data/brain-fit.rds")
 fit <- readRDS("large-data/brain-fit.rds")
 plot(fit,
      y_axis = "group",
@@ -136,4 +228,3 @@ plot(fit,
   coord_cartesian(c(0,1), c(0,5e-5), clip = "off") +
   annotate("text", x = -.14, y = 5.2e-5, fontface="plain",
            label = quote(phantom(0) %*% 10^{-5}), size = 2.5)
-
