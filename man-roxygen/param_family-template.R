fdc8ad7bad7726ab06b2b21d2fb1f6bfa147a825
Daniel J. McDonald <dajmcdon@gmail.com>
2022-09-21 09:01:54 -0700

    refactor main docs

--- man-roxygen/param_family-template.R
+++
@@ -1,4 +0,0 @@
-#' @param family Character. Specifies the loss function to use, valid
-#'   options are:
-#'   * `"gaussian"` - least squares loss (regression, the default),
-#'   * `"binomial"` - logistic loss (classification)
